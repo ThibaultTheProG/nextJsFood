50f2a2129055ee67fca2fbde35e8c03de20fe36b
ThibaultTheProG <tuffinthibaultgw@gmail.com>
2024-09-02 19:58:52 +0200

    Ajout des metas data dynamiques

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import {getMeals} from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared shares by our vibrant community',
+};
+
 async function MealsFetch(){
   const meals = await getMeals();
 
